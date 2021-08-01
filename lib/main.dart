793e4bad1558897b18a683128897c15769ba5b60
chimaisaac60 <51245779+chimaisaac60@users.noreply.github.com>
2021-08-01 03:11:19 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.pink,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My shop'),
+      ),
+      body: Center(
+        child: Text('My shop items are here'),
+      ),
+    );
+  }
+}
